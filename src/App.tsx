796afdf6741d1c17bce01235d7ddd5e74c3a2ce9
TisuPaseo\Imam <imamnajibulloh.in@gmail.com>
2025-11-09 12:31:47 +0700

    Setup UI and dixie js

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1 className="text-lg font-bold m-4">Albion Online Helper</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
